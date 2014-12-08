e35452784fc9959a2f40e594992f07cc60c515b3
Stefanos Anastasiou <stefanos.anastasiou@esrlabs.com>
2014-12-08 15:56:39 +0100

    ---/St.An: Added connector manager class. Added options basic modal dialog.

---
+++ RTextNppPlugin/Forms/Options.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace RTextNppPlugin.Forms
+{
+    partial class Options
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CancelOptionsButton = new System.Windows.Forms.Button();
+            this.SaveOptionsButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CancelOptionsButton
+            // 
+            this.CancelOptionsButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CancelOptionsButton.Location = new System.Drawing.Point(829, 403);
+            this.CancelOptionsButton.Name = "CancelOptionsButton";
+            this.CancelOptionsButton.Size = new System.Drawing.Size(75, 23);
+            this.CancelOptionsButton.TabIndex = 0;
+            this.CancelOptionsButton.Text = "Cancel";
+            this.CancelOptionsButton.UseVisualStyleBackColor = true;
+            // 
+            // SaveOptionsButton
+            // 
+            this.SaveOptionsButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.SaveOptionsButton.Location = new System.Drawing.Point(735, 403);
+            this.SaveOptionsButton.Name = "SaveOptionsButton";
+            this.SaveOptionsButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveOptionsButton.TabIndex = 2;
+            this.SaveOptionsButton.Text = "Save";
+            this.SaveOptionsButton.UseVisualStyleBackColor = true;
+            // 
+            // Options
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 438);
+            this.Controls.Add(this.SaveOptionsButton);
+            this.Controls.Add(this.CancelOptionsButton);
+            this.Name = "Options";
+            this.Text = "Options";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CancelOptionsButton;
+        private System.Windows.Forms.Button SaveOptionsButton;
+    }
+}
