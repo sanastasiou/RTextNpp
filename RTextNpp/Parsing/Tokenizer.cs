65b6fc034fded6133d0f89829b3dedc6b01aea02
Stefanos Anastasiou <stefanos.anastasiou@esrlabs.com>
2015-03-16 21:31:31 +0100

    ---/St.An : Removed installed nuget packages, as they will be downloaded on the fly.

--- RTextNpp/Parsing/Tokenizer.cs
+++ RTextNpp/Parsing/Tokenizer.cs
@@ -1,10 +1,8 @@
-﻿using System;
+﻿using CSScriptIntellisense;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Text.RegularExpressions;
-using CSScriptIntellisense;
 
 namespace RTextNppPlugin.Parsing
 {
