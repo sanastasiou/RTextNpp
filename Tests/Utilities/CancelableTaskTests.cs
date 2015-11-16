45b6ddf2a97e8af72516d9fadab7ac3adbc23406
Stefanos Anastasiou <stefanos.anastasiou@esrlabs.com>
2015-11-16 21:05:45 +0100

    Trying to fix failing test...

--- Tests/Utilities/CancelableTaskTests.cs
+++ Tests/Utilities/CancelableTaskTests.cs
@@ -45,7 +45,7 @@
         public void NormalDelayTest()
         {
             CancelableTask<bool> t = null;
-            Assert.DoesNotThrow(delegate { t = new CancelableTask<bool>(new ActionWrapper<bool, int>(WorkFunction, 100), 200); });
+            Assert.DoesNotThrow(delegate { t = new CancelableTask<bool>(new ActionWrapper<bool, int>(WorkFunction, 100), 2000); });
 
             _retValue = true;
 
@@ -60,7 +60,7 @@
         public void NormalDelayTestTaskExceedsLimit()
         {
             CancelableTask<bool> t = null;
-            Assert.DoesNotThrow(delegate { t = new CancelableTask<bool>(new ActionWrapper<bool, int>(WorkFunction, 300), 200); });
+            Assert.DoesNotThrow(delegate { t = new CancelableTask<bool>(new ActionWrapper<bool, int>(WorkFunction, 1000), 100); });
 
             _retValue = true;
 
