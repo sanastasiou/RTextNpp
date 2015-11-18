512e400b884ad4e3f2a8252dd81ff581ed7c5a39
Stefanos Anastasiou <stefanos.anastasiou@esrlabs.com>
2015-11-19 00:12:15 +0100

    Testing new release system.

--- PushMasterTag.ps1
+++ PushMasterTag.ps1
@@ -1,19 +1,13 @@
-$masterPreparePushCommand = @'
-
-'@
-
 $addMasterTagCommand = @'
-#cmd.exe /C git tag -a -m "RTextNpp Release : v%APPVEYOR_BUILD_VERSION%" v%APPVEYOR_BUILD_VERSION%
-#cmd.exe /C git push origin --tags
+cmd.exe /C git tag -a -m "RTextNpp Release : v%APPVEYOR_BUILD_VERSION%" v%APPVEYOR_BUILD_VERSION%
+cmd.exe /C git push origin --tags
 '@
 
 #execute on different branches than master only
 if($env:appveyor_repo_branch -ne 'master') {
-  # push branch into master
-  Invoke-Expression -Command:$masterPreparePushCommand
   # if commit message has "release" add new tag and push it
-  #if($env:appveyor_repo_commit_message -like "*release*")
-  #{
-  #  Invoke-Expression -Command:$addMasterTagCommand
-  #}
+  if($env:appveyor_repo_commit_message -like "*release*")
+  {
+    Invoke-Expression -Command:$addMasterTagCommand
+  }
 }
