7169c6545a1184b1cda11dd96183f3822e2ce031
Stefanos Anastasiou <stefanos.anastasiou@esrlabs.com>
2015-11-18 12:50:42 +0100

    Trying to check out master...

--- PushMasterTag.ps1
+++ PushMasterTag.ps1
@@ -1,6 +1,6 @@
 $command = @'
 cmd.exe /C git fetch
-cmd.exe /C git checkout master
+cmd.exe /C git checkout -b master --track origin/master
 #cmd.exe /C git pull origin master
 #cmd.exe /C git merge %APPVEYOR_REPO_BRANCH%
 #cmd.exe /C git tag -a master/v%APPVEYOR_BUILD_VERSION%
