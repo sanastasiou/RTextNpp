efe6520e4e750df31ae1e979d463317452dd9313
Stefanos Anastasiou <stefanos.anastasiou@esrlabs.com>
2015-11-18 12:19:30 +0100

    Trying to check out master...

--- PushMasterTag.ps1
+++ PushMasterTag.ps1
@@ -2,9 +2,9 @@ $command = @'
 cmd.exe /C git fetch
 cmd.exe /C git checkout master
 cmd.exe /C git pull origin master
-cmd.exe /C git merge %APPVEYOR_REPO_BRANCH%
-cmd.exe /C git tag -a master/v%APPVEYOR_BUILD_VERSION%
-cmd.exe /C git push origin master/v%APPVEYOR_BUILD_VERSION%
+#cmd.exe /C git merge %APPVEYOR_REPO_BRANCH%
+#cmd.exe /C git tag -a master/v%APPVEYOR_BUILD_VERSION%
+#cmd.exe /C git push origin master/v%APPVEYOR_BUILD_VERSION%
 '@
 
 if($env:appveyor_repo_branch -ne 'master') {
