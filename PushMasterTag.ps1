f2bfe770d8d8c687a5db4c5f38811a7d6a1648e3
Stefanos Anastasiou <stefanos.anastasiou@esrlabs.com>
2015-12-09 15:56:30 +0100

    Prevent merging on normal builds. Added annotation support for multiline annotations. Fixed several UI bugs when multiple workspaces were used. Made workspace loading more robust.

--- PushMasterTag.ps1
+++ PushMasterTag.ps1
@@ -1,4 +1,14 @@
 $addMasterTagCommand = @'
+cmd.exe /C git fetch origin
+cmd.exe /C git remote show origin
+cmd.exe /C git checkout -b master origin/master
+cmd.exe /C git checkout %APPVEYOR_REPO_BRANCH%
+cmd.exe /C git branch -a
+cmd.exe /C git merge --strategy=ours --no-commit master
+cmd.exe /C git commit -m "Merging commit %APPVEYOR_REPO_COMMIT% from branch %APPVEYOR_REPO_BRANCH% - %APPVEYOR_REPO_COMMIT_MESSAGE%"
+cmd.exe /C git checkout master
+cmd.exe /C git merge %APPVEYOR_REPO_BRANCH%
+cmd.exe /C git push origin master
 cmd.exe /C git tag v%APPVEYOR_BUILD_VERSION%
 cmd.exe /C git push origin v%APPVEYOR_BUILD_VERSION%
 '@
